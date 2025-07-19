a9e21c98dae635c363f0875a5cb67f7623991408
jomosen <josemorellsendra@gmail.com>
2025-07-19 11:36:46 +0200

    removed results storage

--- tests/test_google_maps.py
+++ tests/test_google_maps.py
@@ -13,10 +13,26 @@ def scraper_instance():
 
     selenium_driver = SeleniumDriver()
 
-    return GoogleMapsScraper(lang="en", query="boat rental in javea", repository=repository, driver=selenium_driver)
+    return GoogleMapsScraper(lang="en", queries=["boat rental in javea"], repository=repository, driver=selenium_driver)
 
 def test_single_query(scraper_instance):
 
+    scraper_instance.scrape()
+
+    file_path = scraper_instance.repository.file_path
+
+    assert os.path.exists(file_path), f"Expected output CSV not found: {file_path}"
+
+    os.remove(file_path)
+
+def test_batch_queries(scraper_instance):
+
+    queries = [
+        "boat rental in javea",
+        "boat rental in moraira"
+    ]
+
+    scraper_instance.queries = queries
     scraper_instance.scrape()
 
     file_path = scraper_instance.repository.file_path
