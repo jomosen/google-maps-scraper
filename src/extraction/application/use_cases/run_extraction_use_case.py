49c016e9a766c258c442dad36423cbfefe582b3f
jomosen <josemorellsendra@gmail.com>
2025-11-16 11:44:44 +0100

    Add use case

--- src/extraction/application/use_cases/run_extraction_use_case.py
+++ src/extraction/application/use_cases/run_extraction_use_case.py
@@ -1,8 +1,13 @@
+from shared.application.ports.logger_port import LoggerPort
 from shared.application.use_case import UseCase
+from extraction.application.services.extraction_job_runner_service import ExtractionJobRunnerService
+
 
 class RunExtractionUseCase(UseCase):
-    def __init__(self):
-        pass
+
+    def __init__(self, extraction_job_runner: ExtractionJobRunnerService, logger: LoggerPort = None):
+        self.extraction_job_runner = extraction_job_runner
+        self.logger = logger
 
     def execute(self):
         pass
