dbd2e15b15ba7ed9cfd3f2c108101bb2c79abc17
jomosen <josemorellsendra@gmail.com>
2025-11-17 16:57:35 +0100

    Add place extractor port

--- src/extraction/application/ports/place_extractor_port.py
+++ src/extraction/application/ports/place_extractor_port.py
@@ -1,7 +1,15 @@
+from typing import Iterable
 from extraction.domain.job_task import JobTask
-
+from extraction.domain.job_config import JobConfig
+from geonames.domain.geoname import GeoName
+from places.domain.place import Place
 
 class PlaceExtractorPort:
     
-    def extract(self, job_task: JobTask):
+    def extract(self, 
+                job_task: JobTask, 
+                geoname: GeoName,
+                job_config: JobConfig
+                ) -> Iterable[Place]:
+        
         raise NotImplementedError
