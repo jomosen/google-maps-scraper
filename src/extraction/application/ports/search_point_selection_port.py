77ec0d49dddc14b7b240cbeaf84c40e5a5316cec
jomosen <josemorellsendra@gmail.com>
2025-11-20 19:45:07 +0100

    Delete files

--- src/extraction/application/ports/search_point_selection_port.py
+++
@@ -1,8 +0,0 @@
-from typing import Protocol, List, Dict, Any
-from extraction.domain.value_objects.search_point import SearchPoint
-
-
-class SearchPointSelectionPort(Protocol):
-
-    def select(self, filters: Dict[str, Any]) -> List[SearchPoint]:
-        ...
